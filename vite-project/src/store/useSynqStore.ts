31b1e930981cc9131b68a9a54c89665fcdc280e5
Arshita Jaryal <jaryalarshita06@gmail.com>
2026-08-09 15:16:41 +0530

    implement schema CRUD actions in Zustand store

--- vite-project/src/store/useSynqStore.ts
+++ vite-project/src/store/useSynqStore.ts
@@ -3,7 +3,7 @@ import { create } from 'zustand'
 export interface SchemaField {
   id: string
   name: string
-  type: string
+  type: 'uuid' | 'string' | 'email' | 'number' | 'currency' | 'date' | 'boolean' | 'enum' | 'foreign_key'
   options?: string[]
   referenceEntityId?: string
 }
@@ -18,12 +18,68 @@ interface SynqState {
   entities: Entity[]
   generatedData: Record<string, any[]>
   addEntity: (name: string) => void
+  updateEntityName: (id: string, name: string) => void
+  deleteEntity: (id: string) => void
+  addField: (entityId: string, field: Omit<SchemaField, 'id'>) => void
+  updateField: (entityId: string, fieldId: string, updatedField: Partial<SchemaField>) => void
+  deleteField: (entityId: string, fieldId: string) => void
 }
 
 export const useSynqStore = create<SynqState>((set) => ({
   entities: [],
   generatedData: {},
+  
   addEntity: (name) => set((state) => ({
-    entities: [...state.entities, { id: crypto.randomUUID(), name, fields: [] }]
+    entities: [
+      ...state.entities,
+      { id: crypto.randomUUID(), name, fields: [] }
+    ]
+  })),
+
+  updateEntityName: (id, name) => set((state) => ({
+    entities: state.entities.map((ent) => 
+      ent.id === id ? { ...ent, name } : ent
+    )
+  })),
+
+  deleteEntity: (id) => set((state) => ({
+    // Delete the entity and clean up any Foreign Key fields referencing this entity
+    entities: state.entities
+      .filter((ent) => ent.id !== id)
+      .map((ent) => ({
+        ...ent,
+        fields: ent.fields.filter(
+          (field) => !(field.type === 'foreign_key' && field.referenceEntityId === id)
+        )
+      }))
+  })),
+
+  addField: (entityId, field) => set((state) => ({
+    entities: state.entities.map((ent) => 
+      ent.id === entityId 
+        ? { ...ent, fields: [...ent.fields, { ...field, id: crypto.randomUUID() } as SchemaField] } 
+        : ent
+    )
+  })),
+
+  updateField: (entityId, fieldId, updatedField) => set((state) => ({
+    entities: state.entities.map((ent) => 
+      ent.id === entityId 
+        ? {
+            ...ent,
+            fields: ent.fields.map((f) => 
+              f.id === fieldId ? { ...f, ...updatedField } as SchemaField : f
+            )
+          }
+        : ent
+    )
+  })),
+
+  deleteField: (entityId, fieldId) => set((state) => ({
+    entities: state.entities.map((ent) => 
+      ent.id === entityId 
+        ? { ...ent, fields: ent.fields.filter((f) => f.id !== fieldId) } 
+        : ent
+    )
   }))
 }))
