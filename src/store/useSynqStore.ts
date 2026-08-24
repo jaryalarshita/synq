bb5fe83012438533eee0f2fa18c66200ed1ba9df
Arshita Jaryal <jaryalarshita06@gmail.com>
2026-08-24 19:26:39 +0530

    feat: persist schema and generated data to localStorage

--- src/store/useSynqStore.ts
+++ src/store/useSynqStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 export interface SchemaField {
   id: string
@@ -30,77 +31,89 @@ interface SynqState {
   addRecord: (entityId: string, record: any) => void
 }
 
-export const useSynqStore = create<SynqState>((set) => ({
-  entities: [],
-  generatedData: {},
+export const useSynqStore = create<SynqState>()(
+  persist(
+    (set) => ({
+      entities: [],
+      generatedData: {},
 
-  addEntity: (name) => set((state) => ({
-    entities: [
-      ...state.entities,
-      { id: crypto.randomUUID(), name, fields: [] }
-    ]
-  })),
+      addEntity: (name) => set((state) => ({
+        entities: [
+          ...state.entities,
+          { id: crypto.randomUUID(), name, fields: [] }
+        ]
+      })),
 
-  updateEntityName: (id, name) => set((state) => ({
-    entities: state.entities.map((ent) =>
-      ent.id === id ? { ...ent, name } : ent
-    )
-  })),
-
-  deleteEntity: (id) => set((state) => {
-    // Delete the entity and clean up any Foreign Key fields referencing this entity
-    const entities = state.entities
-      .filter((ent) => ent.id !== id)
-      .map((ent) => ({
-        ...ent,
-        fields: ent.fields.filter(
-          (field) => !(field.type === 'foreign_key' && field.referenceEntityId === id)
+      updateEntityName: (id, name) => set((state) => ({
+        entities: state.entities.map((ent) =>
+          ent.id === id ? { ...ent, name } : ent
         )
-      }))
+      })),
 
-    // Also drop any generated records that belonged to the deleted entity
-    const { [id]: _removed, ...generatedData } = state.generatedData
-
-    return { entities, generatedData }
-  }),
-
-  addField: (entityId, field) => set((state) => ({
-    entities: state.entities.map((ent) =>
-      ent.id === entityId
-        ? { ...ent, fields: [...ent.fields, { ...field, id: crypto.randomUUID() } as SchemaField] }
-        : ent
-    )
-  })),
-
-  updateField: (entityId, fieldId, updatedField) => set((state) => ({
-    entities: state.entities.map((ent) =>
-      ent.id === entityId
-        ? {
+      deleteEntity: (id) => set((state) => {
+        // Delete the entity and clean up any Foreign Key fields referencing this entity
+        const entities = state.entities
+          .filter((ent) => ent.id !== id)
+          .map((ent) => ({
             ...ent,
-            fields: ent.fields.map((f) =>
-              f.id === fieldId ? { ...f, ...updatedField } as SchemaField : f
+            fields: ent.fields.filter(
+              (field) => !(field.type === 'foreign_key' && field.referenceEntityId === id)
             )
-          }
-        : ent
-    )
-  })),
+          }))
 
-  deleteField: (entityId, fieldId) => set((state) => ({
-    entities: state.entities.map((ent) =>
-      ent.id === entityId
-        ? { ...ent, fields: ent.fields.filter((f) => f.id !== fieldId) }
-        : ent
-    )
-  })),
+        // Also drop any generated records that belonged to the deleted entity
+        const { [id]: _removed, ...generatedData } = state.generatedData
 
-  isGenerating: false,
-  setGeneratedData: (data) => set({ generatedData: data }),
-  clearGeneratedData: () => set({ generatedData: {} }),
-  setIsGenerating: (isGenerating) => set({ isGenerating }),
-  addRecord: (entityId, record) => set((state) => ({
-    generatedData: {
-      ...state.generatedData,
-      [entityId]: [...(state.generatedData[entityId] || []), record]
+        return { entities, generatedData }
+      }),
+
+      addField: (entityId, field) => set((state) => ({
+        entities: state.entities.map((ent) =>
+          ent.id === entityId
+            ? { ...ent, fields: [...ent.fields, { ...field, id: crypto.randomUUID() } as SchemaField] }
+            : ent
+        )
+      })),
+
+      updateField: (entityId, fieldId, updatedField) => set((state) => ({
+        entities: state.entities.map((ent) =>
+          ent.id === entityId
+            ? {
+                ...ent,
+                fields: ent.fields.map((f) =>
+                  f.id === fieldId ? { ...f, ...updatedField } as SchemaField : f
+                )
+              }
+            : ent
+        )
+      })),
+
+      deleteField: (entityId, fieldId) => set((state) => ({
+        entities: state.entities.map((ent) =>
+          ent.id === entityId
+            ? { ...ent, fields: ent.fields.filter((f) => f.id !== fieldId) }
+            : ent
+        )
+      })),
+
+      isGenerating: false,
+      setGeneratedData: (data) => set({ generatedData: data }),
+      clearGeneratedData: () => set({ generatedData: {} }),
+      setIsGenerating: (isGenerating) => set({ isGenerating }),
+      addRecord: (entityId, record) => set((state) => ({
+        generatedData: {
+          ...state.generatedData,
+          [entityId]: [...(state.generatedData[entityId] || []), record]
+        }
+      }))
+    }),
+    {
+      name: 'synq-storage',
+      // Only persist the schema/data itself, not transient UI flags
+      partialize: (state) => ({
+        entities: state.entities,
+        generatedData: state.generatedData
+      })
     }
-  }))
-}))
+  )
+)
